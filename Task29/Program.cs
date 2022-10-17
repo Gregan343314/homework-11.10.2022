940134f99878a9f92443050070acc75a23e495aa
Gregan <343314@gmail.com>
2022-10-17 21:14:49 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задание №29
+Console.Clear();
+
+int [] numbers = new int[8];
+
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
